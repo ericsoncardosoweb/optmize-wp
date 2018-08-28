8527f482e382ebd4bcc9dd8a214876f0b63001ce
Ericson Cardoso <ericson.cardoso10@gmail.com>
2018-08-28 12:51:40 -0300

    Ajustes de Otimização

--- dpg/security.php
+++ dpg/security.php
@@ -8,6 +8,5 @@ function no_wordpress_errors(){
 add_filter( 'login_errors', 'no_wordpress_errors' );
 
 add_filter('xmlrpc_enabled', '__return_false');
-add_filter('the_generator', create_function(’, 'return "";'));
 remove_action('wp_head', 'rsd_link');
 remove_action('wp_head', 'wlwmanifest_link');
